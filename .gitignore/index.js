f79e6f58e9d9bcf58b4e0dbcc926b8af4908fa52
FireWither <liosanmu@gmail.com>
2019-11-02 16:21:29 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.on('message', message => {
+    if(message.content.startsWith('!dmall')){
+        if(message.member.roles.some(r => r.id === "640107920494690322")){
+            console.log(message.author.mentions)
+            msg=message.content.slice(7)
+            var Count;
+            for(Count in bot.users.array()){
+                var User = bot.users.array()[Count];
+                bot.users.get(User.id).send(msg);
+            }
+            
+        }
+        message.delete(10);
+    }
+    //}
+})
+bot.login('');
